97ee9b0a0f0291175ba49bee7edee41fb565a70a
Naveen Kumar K <naveenkumar.kandasamy@accoliteindia.com>
2018-07-05 16:09:00 +0530

    FTP

---
+++ src/main/java/com/prud/pastel/comm/FTPManager.java
@@ -0,0 +1,58 @@
+package com.prud.pastel.comm;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+public class FTPManager {
+
+	public static void send (File file) {
+        String SFTPHOST = "speedtest.tele2.net";
+        int SFTPPORT = 22;
+        
+        // Destination Server Creds
+        String SFTPUSER = "";
+        String SFTPPASS = "";
+        
+        // Destination folder
+        String SFTPWORKINGDIR = "upload";
+
+        Session session = null;
+        Channel channel = null;
+        ChannelSftp channelSftp = null;
+        System.out.println("preparing the host information for sftp.");
+        try {
+            JSch jsch = new JSch();
+            session = jsch.getSession(SFTPUSER, SFTPHOST, SFTPPORT);
+            session.setPassword(SFTPPASS);
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            session.connect();
+            System.out.println("Host connected.");
+            channel = session.openChannel("sftp");
+            channel.connect();
+            System.out.println("sftp channel opened and connected.");
+            channelSftp = (ChannelSftp) channel;
+            channelSftp.cd(SFTPWORKINGDIR);
+            channelSftp.put(new FileInputStream(file), file.getName());
+            System.out.println("File transfered successfully to host.");
+        } catch (Exception ex) {
+             System.out.println("Exception found while tranfer the response.");
+        }
+        finally{
+
+            channelSftp.exit();
+            System.out.println("sftp Channel exited.");
+            channel.disconnect();
+            System.out.println("Channel disconnected.");
+            session.disconnect();
+            System.out.println("Host Session disconnected.");
+        }
+    }   	
+	
+}
